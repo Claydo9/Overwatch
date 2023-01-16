5992e5d86e5012f24743d0d746e77f6ac587b058
Clayton <claydo9@gmail.com>
2023-01-16 21:35:44 +1000

    Update drawGUI.lua

--- drawGUI.lua
+++ drawGUI.lua
@@ -53,37 +53,3 @@ UIPadding.PaddingBottom = UDim.new( 0.005, 0 )
 UIPadding.PaddingLeft = UDim.new( 0.02, 0 )
 UIPadding.PaddingRight = UDim.new( 0.02, 0 )
 UIPadding.PaddingTop = UDim.new( 0.005, 0 )
-
--- Tempalte
-
-local TemplateStatus = Instance.new( "Frame", StatusContainer )
-TemplateStatus.Name = "TemplateStatus"
-TemplateStatus.BackgroundColor3 = Color3.fromRGB( 53, 53, 53 )
-TemplateStatus.BorderColor3 = Color3.fromRGB( 198 ,198 ,198 )
-TemplateStatus.BorderSizePixel = 1
-
-local Indicator = Instance.new( "Frame", TemplateStatus )
-Indicator.Name = "Indicator"
-Indicator.Size = UDim2.new( 0.1, 0, 0.8, 0 )
-Indicator.Position = UDim2.new( 0.013, 0, 0.095, 0 )
-Indicator.BackgroundColor3 = Color3.fromRGB( 200, 0, 0 )
-Indicator.BorderSizePixel = 0
-
-local StatusToggle = Instance.new( "TextButton", TemplateStatus )
-StatusToggle.Name = "StatusToggle"
-StatusToggle.BackgroundTransparency = 1
-StatusToggle.TextTransparency = 1
-StatusToggle.ZIndex = 5
-StatusToggle.BorderSizePixel = 0
-StatusToggle.Size = UDim2.new( 1, 0, 1, 0 )
-
-local StatusName = Instance.new( "TextLabel", TemplateStatus )
-StatusName.Name = "StatusName"
-StatusName.Text = "Status"
-StatusName.Size = UDim2.new( 0.8, 0, 1, 0 )
-StatusName.Position = UDim2.new( 0.2, 0, 0, 0 )
-StatusName.Font = Enum.Font.Arial
-StatusName.TextSize = 20
-StatusName.TextColor = Color3.fromRGB( 255, 255, 255 )
-StatusName.TextXAlignment = Enum.TextXAlignment.Left
-StatusName.TextWrapped = true
